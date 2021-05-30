b18b72bbc71f2a1bfff3c6d66fe15882c560b2c0
Pavan Yeddanapudi <75864715+pavanscoding@users.noreply.github.com>
2021-05-30 14:09:50 -0700

    Create subsets.py

---
+++ Day 8/Recursion/subsets.py
@@ -0,0 +1,9 @@
+def subsets(num: int, n:int, subset=[]):
+  if(num==n):
+    print(subset)
+  else:
+    subsets(num+1,n,subset)
+    subset.append(num+1)
+    subsets(num+1,n,subset)
+    subset.pop()
+
