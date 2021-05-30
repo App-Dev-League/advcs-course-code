6dd2f49ef83d7df271888726c449f856bc0fc7cf
Pavan Yeddanapudi <75864715+pavanscoding@users.noreply.github.com>
2021-05-30 14:17:39 -0700

    Create planting_problem.py

---
+++ Day 8/Graphs/planting_problem.py
@@ -0,0 +1,17 @@
+#Problem: http://www.usaco.org/index.php?page=viewproblem2&cpid=894
+import sys
+sys.stdin=open("planting.in","r")
+sys.stdout=open("planting.out","w")
+n = int(input())
+degree=[1]*(n+1)
+
+for i in range(n-1):
+  a,b=map(int,input().split())
+  degree[a]+=1
+  degree[b]+=1
+
+ans=0
+for i in degree:
+  ans=max(ans,i)
+
+print(ans)
