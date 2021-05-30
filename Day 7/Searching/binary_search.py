b6ca4aaad96684c3793ed324a1f29e30390c0fba
Pavan Yeddanapudi <75864715+pavanscoding@users.noreply.github.com>
2021-05-30 14:04:32 -0700

    Create binary_search.py

---
+++ Day 7/Searching/binary_search.py
@@ -0,0 +1,18 @@
+import math
+
+def search(arr: list):
+	lo=0
+	hi=len(arr)
+	while(lo<hi):
+		mid=math.ceil((lo+hi)/2)
+		#another way to do the line above
+		#avoids integer overflow
+		mid = lo + math.ceil((hi-lo/2))
+		if(check(mid)):
+			#do something
+			lo=mid or hi=mid-1
+			#the first line depends on the type of the value that you are trying 
+			#to find
+		else:
+			#choose the other option than the one that you chose above
+			lo=mid or hi=mid-1
