110aa2f978ab715f79511ba4a0d67ecb96927a42
Pavan Yeddanapudi <75864715+pavanscoding@users.noreply.github.com>
2021-05-31 10:20:21 -0700

    Update planting_problem.py

--- Day 8/Graphs/planting_problem.py
+++ Day 8/Graphs/planting_problem.py
@@ -6,12 +6,12 @@ n = int(input())
 degree=[1]*(n+1)
 
 for i in range(n-1):
-  a,b=map(int,input().split())
-  degree[a]+=1
-  degree[b]+=1
+    a,b=map(int,input().split())
+    degree[a]+=1
+    degree[b]+=1
 
 ans=0
 for i in degree:
-  ans=max(ans,i)
+    ans=max(ans,i)
 
 print(ans)
