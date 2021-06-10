c6a58d5eaaa7d47d8d75ede1f5ebc26fd6f2e37d
ShafinH <65793752+ShafinH@users.noreply.github.com>
2021-06-09 22:13:02 -0700

    add prediction logic

--- Day 5 & 6/app.py
+++ Day 5 & 6/app.py
@@ -13,7 +13,13 @@ model = load_model('model.h5') # load ML model
 @web_site.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
-		output = ""
+		data = {}
+		dataFrame = pd.DataFrame(data)
+		result = model.predict(dataFrame)
+		if(result == 1):
+			output  = "High chance of heart attack"
+		else:
+			output = "Low chance of heart attack"
 
 		return render_template('index.html', output=output)
 
