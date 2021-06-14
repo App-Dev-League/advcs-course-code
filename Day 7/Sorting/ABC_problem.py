478b5d08f7d31349c39b9049715518724ba163fd
Pavan Yeddanapudi <75864715+pavanscoding@users.noreply.github.com>
2021-06-14 15:01:44 -0700

    Update ABC_problem.py

--- Day 7/Sorting/ABC_problem.py
+++ Day 7/Sorting/ABC_problem.py
@@ -1,6 +1,10 @@
 arr=list(map(int,input().split()))
+#sorting the array makes our lives easier
 arr.sort()
+#since a is the least value, a will be first
 a=arr[0]
+#by the same logic as a, b will be second
 b=arr[1]
+#since a+b+c is the greates value, it will be last. We can use this to find c
 c=arr[6]-a-b
 print(str(a) + " " + str(b) + " " + str(c))
