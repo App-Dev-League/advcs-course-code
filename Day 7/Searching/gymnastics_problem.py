dbf36c5415efc8cea231258d52662b31fdf53ab7
Pavan Yeddanapudi <75864715+pavanscoding@users.noreply.github.com>
2021-06-14 15:00:15 -0700

    Update gymnastics_problem.py

--- Day 7/Searching/gymnastics_problem.py
+++ Day 7/Searching/gymnastics_problem.py
@@ -8,9 +8,12 @@ place1=-1
 place2=-1
 ans = 0
 
+#this function tests how many times cow a has lost to cow b
 def check(a: int, b:int):
 	cnt=0
+	#we iterate all the arrays
 	for i in range(k):
+		#we complete search to find the places of cows a and b
 		for j in range(n):
 			global place1
 			global place2
@@ -18,15 +21,17 @@ def check(a: int, b:int):
 				place1=j
 			if(ses[i][j]==b):
 				place2=j
+		#if cow a has lost to cow b, then we add 1 to the count
 		if(place1<place2):
 			cnt+=1
+	#if cow b has beaten cow a in all of the races, then we add 1 to the answer
 	if(cnt==k):
 		global ans
 		ans+=1
 
 for i in range(k):
 	ses[i]=list(map(int,input().split()))
-
+#we iterate through all possible pairs of the cows
 for i in range(n):
 	for j in range(n):
 		check(i+1, j+1)
