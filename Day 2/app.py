92f629be7fca37c8d53b2dd9caa6e91db14eea41
Kabir R <contact@kabirwearsato.ga>
2021-06-24 18:38:28 -0700

    Update app.py

--- Day 2/app.py
+++ Day 2/app.py
@@ -3,24 +3,18 @@ import logging
 
 app = Flask(__name__)
 app.logger.disabled = True
-log = logging.getLogger('werkzeug')
+log = logging.getLogger("werkzeug")
 log.disabled = True
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def homepage():
-	if request.method == 'POST':
-		name = request.form.get("name")
-		email = request.form.get("email")
-		subject = request.form.get("subject")
-		message = request.form.get("message")
-		print(f"\n============\nNEW MESSAGE!\n\nNAME: {name}\nEMAIL: {email}\nSUBJECT: {subject}\n\nMESSAGE:\n{message}\n============\n")
-		return render_template('sent.html')
-	else:
-		return render_template('index.html')
-
-@app.route('/user/<username>')
-def generate_user(username):
-	return render_template('hello_world.html', user=username)
-
-if __name__ == "__main__":	
-    app.run(host='0.0.0.0', port=8080)
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        subject = request.form.get("subject")
+        message = request.form.get("message")
+        print(f"\n============\nNEW MESSAGE!\n\nNAME: {name}\nEMAIL: {email}\nSUBJECT: {subject}\nMESSAGE:\n{message}\n============\n")
+        return render_template("sent.html", name=name)
+    else:
+        return render_template("index.html")
