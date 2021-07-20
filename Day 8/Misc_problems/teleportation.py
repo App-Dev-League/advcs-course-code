9580476474f9c78ca77fa6806d1df5a0f7bde7cd
Pavan Yeddanapudi <75864715+pavanscoding@users.noreply.github.com>
2021-07-20 11:01:43 -0700

    Update teleportation.py

--- Day 8/Misc_problems/teleportation.py
+++ Day 8/Misc_problems/teleportation.py
@@ -1,1 +1,16 @@
+import sys 
+sys.stdin=open("teleport.in","r")
+sys.stdout=open("teleport.out","w")
+a,b,x,y=map(int,input().split())
+
+if(a>b):
+    a,b=b,a
+if(x>y):
+    x,y=y,x
+
+norm_dist=abs(a-b)
+teleport_dist=abs(x-a)+abs(b-y)
+ans=min(norm_dist,teleport_dist)
+print(ans)
+
 
