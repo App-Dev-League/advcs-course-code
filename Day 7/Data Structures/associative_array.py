18e7d27d079df14609d1b146a140b37d7446b4e9
Pavan Yeddanapudi <75864715+pavanscoding@users.noreply.github.com>
2021-07-20 10:07:43 -0700

    Update associative_array.py

--- Day 7/Data Structures/associative_array.py
+++ Day 7/Data Structures/associative_array.py
@@ -1,1 +1,13 @@
-
+data={}
+q=int(input())
+while(q>0):
+    stuff=list(map(int,input().split()))
+    query=stuff[0]
+    if(query):
+        if(stuff[1] in data.keys()):
+            print(data[stuff[1]])
+        else:
+            print(0)
+    else:
+        data[stuff[1]]=stuff[2]
+    q-=1
