99e16542165fa376f8d9e3a3c2e6695bd31149f2
Pavan Yeddanapudi <75864715+pavanscoding@users.noreply.github.com>
2021-07-20 10:53:43 -0700

    Update maximum_distance.py

--- Day 8/Misc_problems/maximum_distance.py
+++ Day 8/Misc_problems/maximum_distance.py
@@ -1,1 +1,10 @@
+n=int(input())
+coordx=[int(x) for x in input().split()]
+coordy=[int(x) for x in input().split()]
+
+ans=0
+for i in range(n):
+    for j in range(n):
+        ans=max((coordx[i]-coordx[j])**2+(coordy[i]-coordy[j])**2,ans)
+print(ans)
 
