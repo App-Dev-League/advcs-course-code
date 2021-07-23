bb56e424880969afd57bcbacaf0ed3a6150651ec
Pavan Yeddanapudi <75864715+pavanscoding@users.noreply.github.com>
2021-07-22 17:13:08 -0700

    Update permutation.py

--- Day 8/Recursion/permutation.py
+++ Day 8/Recursion/permutation.py
@@ -3,8 +3,8 @@ def permute(n:int,num=1, per=[]):
         print(per)
     else:
         for i in range(n):
-        if((i+1) in per):
-            continue
+            if(not((i+1) in per)):
+                break
         else:
             per.append(i+1)
             permute(n,num+1,per)
